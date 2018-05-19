cf05a62153dd7b00b54a5a72c7563f59390440c0
Miha <zadravec.miha96@gmail.com>
2018-05-19 14:59:35 +0200

    js css

--- Views/Layout/upper.php
+++ Views/Layout/upper.php
@@ -1,5 +1,15 @@
 <html>
 	<head>
+		<meta charset="utf-8">
+		
+		<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+		
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-blue-grey.css">
+		<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans'>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+		
 		<title>Test</title>
 	</head>
 	<body>
